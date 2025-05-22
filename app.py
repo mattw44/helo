8ee0ae75ac62db7e98e0edd2ba2d8d183a9f2973
mattw44 <supik713@gmail.com>
2025-05-22 08:48:58 +0700

    Update app.py

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 st.title("Hello, World")
 st.write("Ini adalah aplikasi Streamlit pertama saya yang dijalankan melalui GitHub!")
-st.image("https://akcdn.detik.net.id/community/media/visual/2025/05/05/jokowi-1746424405213_169.jpeg?w=700&q=90", caption=" YNTKTS", use_column_width=True)
+
