d4b91dfa43674709461eb4c3ddb63ce9a8b4dc56
mattw44 <supik713@gmail.com>
2025-05-22 09:24:01 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.title("Hello, World!")
-st.write("Ini adalah aplikasi Streamlit pertama saya yang dijalankan melalui GitHub!")
-
+st.set_page_config(page_title="Demo Menu", layout="centered")
 st.sidebar.header("Project1")
