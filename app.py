9252ce6ebeea8fbd2684e9a97ff996c09455c9d1
mattw44 <supik713@gmail.com>
2025-05-22 09:13:33 +0700

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,4 @@ st.title("Hello, World")
 st.write("Ini adalah aplikasi Streamlit pertama saya yang dijalankan melalui GitHub!")
 
 st.set_page_config(page_title="Project1", layout="centerd")
+st.sidebar.header("Project1")
