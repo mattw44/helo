687f67c8a7f60bafc01e44e8dac89ad9a72b899a
mattw44 <supik713@gmail.com>
2025-05-22 08:45:12 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,5 @@
 import streamlit as st
 
-st.title("YNTKTS")
+st.title("Hello, World")
 st.write("Ini adalah aplikasi Streamlit pertama saya yang dijalankan melalui GitHub!")
+st.image("https://www.google.com/imgres?q=kucing%20stay%20halal&imgurl=https%3A%2F%2Fi.pinimg.com%2F736x%2F5c%2F07%2F40%2F5c0740496e8d4b58c82d20ccc992888e.jpg&imgrefurl=https%3A%2F%2Fid.pinterest.com%2Fpin%2F887068457843446356%2F&docid=o8PyBF65YBVuaM&tbnid=Uff_J5ytO9kIpM&vet=12ahUKEwjOqaLu-bWNAxWzxTgGHfwaJekQM3oECFcQAA..i&w=720&h=720&hcb=2&ved=2ahUKEwjOqaLu-bWNAxWzxTgGHfwaJekQM3oECFcQAA", caption=" JAWA!", use_column_width=True)
