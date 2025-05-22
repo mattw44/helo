fa148421e534cac9efa6f18d6978e42517c2e784
mattw44 <supik713@gmail.com>
2025-05-22 09:27:13 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 import streamlit as st
 
-st.set_page_config(page_title="Demo Menu", layout="centered")
-st.sidebar.header("Project1")
+st.set_page_config(page_title="Aplikasi Multi-Halaman", layout="centered")
+
+st.title("Selamat Datang di Aplikasi Streamlit")
+st.write("Gunakan menu di sebelah kiri untuk navigasi antar halaman.")
