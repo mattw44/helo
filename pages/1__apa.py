ea208e138360c25b3a3a6386da537dfb3f5c932f
mattw44 <supik713@gmail.com>
2025-05-22 09:28:55 +0700

    Update 1__apa.py

--- pages/1__apa.py
+++ pages/1__apa.py
@@ -1,9 +1,6 @@
 import streamlit as st
-import pandas as pd
-import joblib
-from sklearn.model_selection import train_test_split
-from sklearn.metrics import classification_report,accuracy_score
 
-st.set_page_config(page_title="Main")
-st.title("Main")
+st.title("Tentang Aplikasi")
+st.write("Ini adalah halaman tentang dari aplikasi multi-halaman menggunakan Streamlit.")
+
 st.sidebar.header("Main")
