747023e85740a0419f9931d0e78045414922d9ca
mattw44 <supik713@gmail.com>
2025-05-22 09:27:46 +0700

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,4 @@
 import streamlit as st
 
-st.set_page_config(page_title="Aplikasi Multi-Halaman", layout="centered")
-
-st.title("Selamat Datang di Aplikasi Streamlit")
-st.write("Gunakan menu di sebelah kiri untuk navigasi antar halaman.")
+st.title("Tentang Aplikasi")
+st.write("Ini adalah halaman tentang dari aplikasi multi-halaman menggunakan Streamlit.")
